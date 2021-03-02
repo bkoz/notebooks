efa10e910ca445cb81697314d5b100c598e55f66
Bob Kozdemba <bkozdemba@gmail.com>
2021-03-01 19:12:02 -0600

    updated

--- 13-pytorch-cuda-info.py
+++ 13-pytorch-cuda-info.py
@@ -2,6 +2,10 @@ import torch
 
 torch.cuda.init()
 
+print('Torch version: ' + torch.__version__)
+print('CUDA available: ' + str(torch.cuda.is_available()))
+print('cuDNN version: ' + str(torch.backends.cudnn.version()))
+
 if torch.cuda.is_initialized():
     for i in range(torch.cuda.device_count()):
         dev_str = f'cuda:{i}'
