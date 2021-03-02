6557315241ba27f07a5607afffdf9addd3402577
Bob Kozdemba <bkozdemba@gmail.com>
2021-03-01 20:28:23 -0600

    updated

--- 13-pytorch-cuda-info.py
+++ 13-pytorch-cuda-info.py
@@ -2,17 +2,18 @@ import torch
 
 try:
     torch.cuda.init()
-except:
-    AssertionError
 
     print('Torch version: ' + torch.__version__)
     print('CUDA available: ' + str(torch.cuda.is_available()))
     print('cuDNN version: ' + str(torch.backends.cudnn.version()))
 
-    if torch.cuda.is_initialized():
+    try:
+        torch.cuda.is_initialized()
         for i in range(torch.cuda.device_count()):
             dev_str = f'cuda:{i}'
             print(torch.cuda.get_device_properties(dev_str))
-    else:
-        print('CUDA is not available!')
-
+    except:
+        print('Can not init CUDA!')
+        assert AssertionError
+except:
+    print('CUDA is not available')
